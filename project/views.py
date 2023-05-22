36a418a43a7e3ee31c67e29696fbaba9c7cefce6
shashanksjSSJ <shashanksj26@gmail.com>
2023-05-22 19:14:14 +0530

    New Commit

--- project/views.py
+++ project/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, redirect,HttpResponseRedirect
+from django.shortcuts import render, redirect, HttpResponseRedirect
 from .forms import SignUp, LoginForm, UploadDocument, announcmentform, StudentEmailForm, parentemailform
 from django.contrib.auth import authenticate, login, logout
-from .models import StudentData, uploaddata, StudentData1, Announcements1,Student, Parent
+from .models import StudentData, uploaddata, StudentData1, Announcements1, Student, Parent
 from django.core.mail import send_mail
 from django.conf import settings
 # Create your views here.
+
 
 def index(request):
     return render(request, 'index.html')
@@ -15,16 +16,23 @@ def register(request):
     if request.method == 'POST':
         form = SignUp(request.POST)
         if form.is_valid():
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+            temp1 = form.cleaned_data.get('is_student')
+            temp2 = form.cleaned_data.get('is_staff')
             data = form.cleaned_data
             emil = data['email']
-            user = form.save()
-            msg = 'user created'
-            return redirect('loginpage')
+            if Student.objects.filter(reg_no=username, email=email).exists():
+                user = form.save()
+                msg = 'user created'
+                return redirect('loginpage')
+            else:
+                msg = 'USN is not present'
         else:
             msg = 'form is invalid'
     else:
         form = SignUp()
-    return render(request,'register.html',{'form': form, 'msg': msg})
+    return render(request, 'register.html', {'form': form, 'msg': msg})
 
 
 def loginpage(request):
@@ -66,42 +74,47 @@ def students(request):
             rno = fm.cleaned_data.get('regno')
             sub = fm.cleaned_data.get('subject')
             lnk = fm.cleaned_data.get('link')
-            sav = uploaddata(subject=sub,regno=rno,link=lnk)
+            sav = uploaddata(subject=sub, regno=rno, link=lnk)
             sav.save()
             fm = UploadDocument()
     else:
         fm = UploadDocument()
 
-    return render(request, 'students.html',{'form':fm,'up':up ,'cu':cu, 't':temp})
+    return render(request, 'students.html', {'form': fm, 'up': up, 'cu': cu, 't': temp})
 
 
 def staff(request):
     if request.method == 'GET':
-        t = request.GET.get('search',None)
+        t = request.GET.get('search', None)
         try:
-            temp = uploaddata.objects.filter(regno__icontains=t).order_by('-id')
+            temp = uploaddata.objects.filter(
+                regno__icontains=t).order_by('-id')
         except:
             temp = uploaddata.objects.all().order_by('-id')
     else:
         temp = uploaddata.objects.all().order_by('-id')
-    
-    return render(request, 'staff.html', {'up':temp})
+
+    return render(request, 'staff.html', {'up': temp})
+
 
 def studentd(request):
     sd = StudentData1.objects.order_by('regno')
-    return render(request, 'studentdata.html',{'sd':sd})
+    return render(request, 'studentdata.html', {'sd': sd})
+
 
 def calendar(request):
-    return render(request,'calendar.html')
+    return render(request, 'calendar.html')
+
 
 def attendence(request):
     currnet_user = request.user
-    cu = currnet_user.username 
+    cu = currnet_user.username
     msg = 'NO Found'
     sd = StudentData1.objects.all()
     sub = ['Software Engineering', 'Database Design']
-    return render(request,'attendence.html',{'msg':msg, 'mo':sd, 'cu':cu, 'sub':sub})
-        
+    return render(request, 'attendence.html', {'msg': msg, 'mo': sd, 'cu': cu, 'sub': sub})
+
+
 def announce(request):
     temp = Announcements1.objects.all().order_by('-date')
     if request.method == 'POST':
@@ -109,15 +122,17 @@ def announce(request):
         if fm.is_valid():
             sub = fm.cleaned_data.get('subject')
             tex = fm.cleaned_data.get('text')
-            sav = Announcements1(subject=sub,text=tex)
+            sav = Announcements1(subject=sub, text=tex)
             sav.save()
     else:
         fm = announcmentform()
-    return render(request,'staffann.html', {'t':temp, 'form':fm})
+    return render(request, 'staffann.html', {'t': temp, 'form': fm})
+
 
 def stannounce(request):
     temp = Announcements1.objects.all().order_by('-date')
-    return render(request, 'studentannouncements.html' , {'t':temp})
+    return render(request, 'studentannouncements.html', {'t': temp})
+
 
 def studentmail(request):
     if request.method == 'POST':
@@ -139,6 +154,7 @@ def studentmail(request):
     students = Student.objects.all()
     return render(request, 'sendmailstudents.html', {'form': form, 'students': students})
 
+
 def parentmail(request):
     if request.method == 'POST':
         form = parentemailform(request.POST)
@@ -157,7 +173,8 @@ def parentmail(request):
     else:
         form = parentemailform()
     students = Parent.objects.all()
-    return render(request,'parentemailform.html', {'form': form, 'students': students} )
+    return render(request, 'parentemailform.html', {'form': form, 'students': students})
+
 
 def user_logout(request):
     logout(request)
